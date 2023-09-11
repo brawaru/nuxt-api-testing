69e5ab321c6bcf78d3e9538a88083a2f57aef608
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-09-11 05:21:56 +0200

    Implement motd display

--- app.vue
+++ app.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <template v-if="status === 'success'">
+      <div class="message-contents">{{ motd!.text }}</div>
+      <div class="message-date">
+        {{
+          new Date(motd!.lastUpdatedAt).toLocaleString("en-US", {
+            dateStyle: "medium",
+            timeStyle: "short",
+          })
+        }}
+      </div>
+    </template>
+    <template v-else-if="status === 'pending'">Loading motd...</template>
+    <template v-else-if="status === 'error'">
+      Failed to load motd. {{ String(error) }}
+    </template>
+    <template v-else-if="status === 'idle'">
+      Waiting for motd to start loading...
+    </template>
   </div>
 </template>
+<script setup lang="ts">
+const {
+  data: motd,
+  status,
+  error,
+} = await useFetch("/api/motd", {
+  transform: ({ motd }) => motd,
+});
+</script>
+<style scoped>
+.message-contents {
+  font-size: 1.2rem;
+}
+
+.message-date {
+  font-style: italic;
+}
+</style>
